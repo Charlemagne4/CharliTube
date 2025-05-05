904f225c5fa8df369ca09ad780da592c1d221878
Charlemagne4 <mohdingo4@gmail.com>
2025-05-05 17:57:59 +0100

    implement prisma with mongodb and next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,9 +3,14 @@
 import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import DiscordProvider from 'next-auth/providers/discord';
+
 import { env } from '@/data/server';
 
+import { prisma } from '../../../../../prisma/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: env.GITHUB_CLIENT_ID,
@@ -16,7 +21,10 @@ export const authOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET
     })
     // Add other providers as needed
-  ]
+  ],
+  session: {
+    strategy: 'jwt' as const // Use JWT for session management
+  }
   // Additional NextAuth.js configuration options
 };
 
