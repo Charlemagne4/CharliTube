d735df6a5e034bbac7882306174c72345b349e63
Charlemagne4 <mohdingo4@gmail.com>
2025-06-05 17:45:34 +0100

    Reminder(auth)/to update user image provider in the db when login

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
 // app/api/auth/[...nextauth]/route.js
-import { Session, User } from 'next-auth';
+import { Session, User, Account, Profile } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import DiscordProvider from 'next-auth/providers/discord';
 
@@ -13,17 +13,34 @@ export const authOptions = {
   providers: [
     GitHubProvider({
       clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET
+      clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET
-    })
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
   ],
   session: {
-    strategy: 'jwt' as const // Use JWT for session management
+    strategy: 'jwt' as const, // Use JWT for session management
   },
   callbacks: {
+    //TODO: update user Image from provider when he login
+    // async signIn({ user }: { user: User; account: Account | null; profile?: Profile }) {
+    //   // Optional: Only update if image has changed
+    //   const existingUser = await prisma.user.findUnique({
+    //     where: { email: user.email || undefined },
+    //   });
+    //   console.log(existingUser);
+
+    //   if (existingUser && existingUser.image !== user.image) {
+    //     await prisma.user.update({
+    //       where: { email: user.email || undefined },
+    //       data: { image: user.image },
+    //     });
+    //   }
+
+    //   return true;
+    // },
     async jwt({ token, user }: { token: JWT; user: User }) {
       if (user) {
         token.role = user.role;
@@ -36,6 +53,6 @@ export const authOptions = {
       session.user.role = token.role as string;
       session.user.id = token.id as string;
       return session;
-    }
-  }
+    },
+  },
 };
