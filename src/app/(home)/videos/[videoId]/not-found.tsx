1abc8c6851854ffbbe33b8f46a167e0ad3566e77
Charlemagne4 <mohdingo4@gmail.com>
2025-06-18 19:54:20 +0100

    fix(video): crash on invalid video ID handled with not found page

---
+++ src/app/(home)/videos/[videoId]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function VideoNotFound() {
+  return (
+    <div style={{ padding: 40, textAlign: 'center' }}>
+      <h1>Video not found 😞</h1>
+      <p>The video you are looking for does not seem to exist.</p>
+    </div>
+  );
+}
--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -1,7 +1,10 @@
 import { DEFAULT_LIMIT } from '@/constants';
 import VideoView from '@/modules/videos/views/VideoView';
 import { HydrateClient, trpc } from '@/trpc/server';
+import { TRPCClientError } from '@trpc/client';
+import { TRPCError } from '@trpc/server';
 import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
@@ -17,8 +20,14 @@ async function page({ params }: pageProps) {
   }
   const { videoId } = await params;
 
-  void trpc.videos.getOne.prefetch({ videoId });
-  //TODO: change to prefetchInfinite when implementing pagination
+  // 1️⃣ Check if video exists
+  try {
+    await trpc.videos.getOne({ videoId });
+  } catch (err) {
+    // Catch TRPC 'NOT_FOUND' error and call notFound()
+    notFound()
+  }
+  // void trpc.videos.getOne.prefetch({ videoId });
   void trpc.comments.getMany.prefetchInfinite({ videoId, limit: DEFAULT_LIMIT });
   void trpc.reactions.getOne.prefetch({ videoId });
   void trpc.suggestions.getMany.prefetchInfinite({ videoId, limit: DEFAULT_LIMIT });
